694f8dde3e2abf15ee80165d6cf4cf288e0c8aa0
meljaprem <meljaprem@gmail.com>
2017-09-08 11:43:43 +0300

    change mongoConfig

--- src/main/java/com/prem/studyproject/config/MongoConfig.java
+++ src/main/java/com/prem/studyproject/config/MongoConfig.java
@@ -1,18 +1,15 @@
 package com.prem.studyproject.config;
 
 
-import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
-import com.mongodb.MongoCredential;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
@@ -20,22 +17,30 @@ import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 @Configuration
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 @ToString
-@ConfigurationProperties("mongodb")
+//@ConfigurationProperties("mongodb")
 @Slf4j
 public class MongoConfig {
+    @Value("${DB_HOST}")
     private String host;
-    private Integer port;
+    @Value("${DB_PORT}")
+    private String port;
+    @Value("${DB_USER}")
     private String user;
+    @Value("${DB_PASSWORD}")
     private String pass;
+    @Value("${DB_NAME}")
     private String database;
-
-    Environment environment;
 
 
     public @Bean
-    MongoDbFactory mongoDbFactory2() throws Exception {
+    MongoDbFactory mongoDbFactory(
+//            @Value("${DB_HOST}") String host,
+//            @Value("${DB_PORT}") String port,
+//            @Value("${DB_USER}") String user,
+//            @Value("${DB_PASSWORD}") String pass,
+//            @Value("${DB_NAME}") String database
+    ) throws Exception {
         this.toString();
         String prefix = "mongodb://";
         String connectionURI = host + ":" + port + "/" + database;
@@ -48,11 +53,6 @@ public class MongoConfig {
         }
         log.info("Connected to MongoDB URI: " + uri.toString());
         return new SimpleMongoDbFactory(uri);
-    }
-
-    public @Bean
-    MongoDbFactory mongoDbFactory() throws Exception {
-        return new SimpleMongoDbFactory(new MongoClient(host, port), database);
     }
 
 
